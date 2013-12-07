623ecca3fb8a79e91343c3268c662c3002863d9f
fabiogalo <fabio.galo.dominguez@hotmail.com>
2013-12-07 11:54:23 -0600

    Geolocaliza
    Geolocaliza

---
+++ js/actions.js
@@ -0,0 +1,28 @@
+// actions.js
+function dibujarMapa(lat, lon) {
+	//Posición del mapa
+	var latlng = new google.maps.LatLng(lat, lon);
+	var myOptions = {
+		zoom: 12,
+		center: latlng,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+	};
+	var map = new google.maps.Map(document.getElementById("map_canvas"),myOptions);
+	//Marcador
+	var marker = new google.maps.Marker({
+		position: latlng, 
+		map: map, 
+		title:"Mi posición"
+	});
+}
+
+$(function(){
+	document.addEventListener("deviceready", function(){
+		navigator.geolocation.getCurrentPosition(function(position){
+			dibujarMapa(position.coords.latitude, position.cords.longitude);
+		}, function(error) {
+		alert('code: '    + error.code    + '\n' +
+		  'message: ' + error.message + '\n');
+		});
+	},false);
+});
